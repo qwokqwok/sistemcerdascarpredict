3a8c0e4e1ff85fa7f2e893d96aa61dd30c534353
qwokqwok <124016300+qwokqowk@users.noreply.github.com>
2023-12-01 20:15:50 +0700

    no message

--- pages/1_nomer8.py
+++ pages/1_nomer8.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
-import numpy as np
+import numpy as npd
 import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
